3dbf9a969dbe754402d8496f4b91fe5e9b16d39b
Fernando Jiménez <fer@fer.xyz>
2025-07-13 22:34:40 +0100

    Authorized keys

--- hosts/common/nixos/users.nix
+++ hosts/common/nixos/users.nix
@@ -12,7 +12,7 @@
       extraGroups = ["wheel" "networkmanager" "audio" "video" "docker"];
       shell = pkgs.fish;  # Changed default shell to fish
       openssh.authorizedKeys.keys = [
-        # Add your SSH public keys here
+        ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQ5a7p+8LrnKTch/UUAJ3YpAYT6PS8fM+0FKtSspZ5U
       ];
     };
   };
