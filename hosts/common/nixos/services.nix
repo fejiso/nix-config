bac44cf0ba5ce4c7bbf813780d33acfe39e932da
Fernando Jiménez <fer@fer.xyz>
2025-07-14 08:42:52 +0100

    GPG setup

--- hosts/common/nixos/services.nix
+++ hosts/common/nixos/services.nix
@@ -14,13 +14,13 @@
       KbdInteractiveAuthentication = false;
     };
   };
-  
+
   # Enable SSH agent
   programs.ssh.startAgent = true;
-  
+
   # Enable CUPS for printing
   services.printing.enable = true;
-  
+
   # Enable sound with pipewire
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -30,12 +30,20 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-  
+
   # Enable location services
   services.geoclue2.enable = true;
 
-  
-  
+  #gpg
+  # on your server's configuration.nix
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-curses;
+    settings = {
+      enable-ssh-support = true;
+    };
+  };
+  services.pcscd.enable = true;
   # Enable automatic login for the user
   # services.getty.autologinUser = "superfer";
 }
