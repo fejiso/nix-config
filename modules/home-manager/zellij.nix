e327d72629dec32df9f386f2f6bc4910777a276e
Fernando Jiménez <fer@fer.xyz>
2025-08-20 22:17:16 +0100

    Fixed zellij once and for all

--- modules/home-manager/zellij.nix
+++ modules/home-manager/zellij.nix
@@ -1,66 +1,40 @@
 { config, pkgs, ... }:
 
 {
-  programs.zellij = {
-    enable = true;
-    settings = {
-      scroll_buffer_size = 100000;
-      ui = {
-        pane_frames = {
-          rounded_corners = true;
-        };
-      };
-      theme = "custom";
-      themes.custom.fg = "#ffffff";
-      keybinds = {
-        
-        normal = {
-          unbind = ["Alt Left" "Alt Right" "Alt Up" "Alt Down"];
-        };
-        tab = {
-          unbind = ["Ctrl t"];
-          bind = {
-            "Ctrl y" = { action = { SwitchToMode._args = ["Normal"]; }; };
-          };
-        };
-        pane._children = [
-          {
-            bind = {
-              _args = ["e"];
-              _children = [
-                { TogglePaneEmbedOrFloating = {}; }
-                { SwitchToMode._args = ["locked"]; }
-              ];
-            };
-          }
-          {
-            bind = {
-              _args = ["left"];
-              MoveFocus = ["left"];
-            };
-          }
-        ];
-        # The original `shared_except` cannot be directly translated to the new format
-        # without changing its behavior, as the new `shared_except` structure is different.
-        # Please provide guidance on how you'd like to re-implement this logic.
-        # Original shared_except:
-        # shared_except = [
-        #   {
-        #     except = ["tab" "locked"];
-        #     unbind = ["Ctrl t"];
-        #     binds = {
-        #       "Ctrl y" = { SwitchToMode = "Tab"; };
-        #     };
-        #   }
-        #   {
-        #     except = ["move" "locked"];
-        #     unbind = ["Ctrl h"];
-        #     binds = {
-        #       "Ctrl j" = { SwitchToMode = "Move"; };
-        #     };
-        #   }
-        # ];
-      };
-    };
-  };
+  programs.zellij.enable = true;
+  
+  home.file.".config/zellij/config.kdl".text = ''
+    scroll_buffer_size 100000
+
+    ui {
+        pane_frames {
+                    rounded_corners true
+        }
+    }
+
+    keybinds {
+        normal {
+            // used for moving in fish, hjkl is enough
+            unbind "Alt Left"
+            unbind "Alt Right"
+            unbind "Alt Up"
+            unbind "Alt Down"
+        }
+        tab {
+            // used for fzf in fish
+           unbind "Ctrl t"
+           bind "Ctrl y" { SwitchToMode "Normal"; }
+        }
+        shared_except "tab" "locked" {
+            // used for fzf in fish
+           unbind "Ctrl t"
+           bind "Ctrl y" { SwitchToMode "Tab"; }
+        }
+        shared_except "move" "locked" {
+            // collides with ctrl+backspace
+            unbind "Ctrl h"
+            bind "Ctrl j" { SwitchToMode "Move"; }
+        }
+    }
+  '';
 }
