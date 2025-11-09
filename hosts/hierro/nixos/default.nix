f9f742f1a2676a88bdf990377053697e4cb56131
Fernando Jiménez <fer@fer.xyz>
2025-11-09 10:14:48 +0000

    hierro

---
+++ hosts/hierro/nixos/default.nix
@@ -0,0 +1,40 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  hostname,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ../../common/nixos
+  ];
+
+  # Boot configuration
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.systemd.enable = true;
+  boot.initrd.compressor = "xz";
+  
+  # Disable desktop/wayland services for server
+  services.xserver.enable = lib.mkForce false;
+  services.displayManager.sddm.enable = lib.mkForce false;
+  hardware.pulseaudio.enable = lib.mkForce false;
+  services.pipewire.enable = lib.mkForce false;
+  hardware.bluetooth.enable = lib.mkForce false;
+  
+  # Network configuration
+  networking.hostName = "hierro";
+  
+  # Override SSH settings for server access
+  services.openssh.settings = {
+    PermitRootLogin = lib.mkForce "yes";
+    PasswordAuthentication = lib.mkForce true;
+    KbdInteractiveAuthentication = lib.mkForce true;
+  };
+  
+  # System state version
+  system.stateVersion = "25.05";
+}
