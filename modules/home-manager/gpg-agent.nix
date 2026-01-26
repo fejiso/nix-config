c9d62ef6d13da68cb500e06295f70915958c7c7f
Fernando Jiménez <fer@fer.xyz>
2026-01-26 15:44:29 +0100

    gpg nonsense

--- modules/home-manager/gpg-agent.nix
+++ modules/home-manager/gpg-agent.nix
@@ -1,17 +1,16 @@
 {
   pkgs, ...
 }: {
-  home.packages = [ pkgs.pinentry-curses ];
-  
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentry.package = pkgs.pinentry-curses;
+    pinentryPackage = pkgs.pinentry-tty;
     defaultCacheTtl = 86400;  # 24 hours
     maxCacheTtl = 86400;      # 24 hours
     extraConfig = ''
       allow-loopback-pinentry
       allow-preset-passphrase
+      no-grab
     '';
   };
 }
