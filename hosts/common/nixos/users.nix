edba96602cdf4dff6a2999321fc17bf5882b99e3
Fernando Jiménez <fer@fer.xyz>
2026-01-27 10:35:27 +0100

    group

--- hosts/common/nixos/users.nix
+++ hosts/common/nixos/users.nix
@@ -9,7 +9,7 @@
     z-247 = {
       isNormalUser = true;
       description = "z-247";
-      extraGroups = ["wheel" "networkmanager" "audio" "video" "docker" "pipewire"];
+      extraGroups = ["wheel" "networkmanager" "audio" "video" "docker" "pipewire" "cdrom"];
       shell = pkgs.fish;  # Changed default shell to fish
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQ5a7p+8LrnKTch/UUAJ3YpAYT6PS8fM+0FKtSspZ5U"
