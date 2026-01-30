9dfbc359dde4a9a1012684dd07935f15407f3eb5
Fernando Jiménez <fer@fer.xyz>
2026-01-30 14:07:01 +0100

    warns

--- hosts/common/home/default.nix
+++ hosts/common/home/default.nix
@@ -117,12 +117,9 @@
   # Swayidle configuration
   services.swayidle = lib.mkIf (!config.programs.niri.enable) {
     enable = true;
-    events = [
-      {
-        event = "before-sleep";
-        command = "${pkgs.hyprlock}/bin/hyprlock";
-      }
-    ];
+    events = {
+      before-sleep = "${pkgs.hyprlock}/bin/hyprlock";
+    };
     timeouts = [
       {
         timeout = 600;
