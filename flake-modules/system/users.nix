98e118ae6bd2163a862019f171fa8e7fe2f45af6
Fernando Jiménez <fer@fer.xyz>
2026-09-09 23:07:14 +0100

    bcachefs patches

--- flake-modules/system/users.nix
+++ flake-modules/system/users.nix
@@ -11,7 +11,7 @@
     z-247 = {
       isNormalUser = true;
       description = "z-247";
-      extraGroups = ["wheel" "networkmanager" "audio" "video" "docker" "pipewire" "cdrom" "dialout" "tty" "scanner" "lp"];
+      extraGroups = ["wheel" "networkmanager" "audio" "video" "docker" "pipewire" "cdrom" "dialout" "tty" "scanner" "lp" "kvm"];
       shell = pkgs.fish;  # Changed default shell to fish
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFihyYRTqPTs7dzyirRCxGrkFuV5ymWkCJp2uPLgJ/7o username@daremote"
